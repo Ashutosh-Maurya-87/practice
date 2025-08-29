019b4e31f6b4515e8b60b10d75be0163dd630844
Ashutosh Maurya <ashumaurya486@gmail.com>
2025-08-29 11:26:56 +0530

    leet code prob 88 merginf two sorted array with given length

--- MergeSortedArray.js
+++ MergeSortedArray.js
@@ -1,7 +1,17 @@
 // leet code prob -88
 
 const MergeSortedArray = (a1, a2, a1Length, a2Length) => {
-
+    let m = a1Length - 1, n = a2Length - 1
+    for (let i = a1.length - 1; i >= 0; i--) {
+        if (a1[m] >= a2[n]) {
+            a1[i] = a1[m]
+            m--
+        } else if (a2Length >= 0) {
+            a1[i] = a2[n]
+            n--
+        }
+    }
+    return a1
 }
 
-console.log(MergeSortedArray([1,2,3,0,0,0], [2,5,6], 3, 3))
+console.log(MergeSortedArray([1, 2, 3, 0, 0, 0], [2, 5, 6], 3, 3))
