7f17d65a954cc0d747b16b4decde4425e54ad737
Ashutosh Maurya <ashumaurya486@gmail.com>
2025-09-01 07:27:33 +0530

    Leet code prob solve 268

--- MissingNumber.js
+++ MissingNumber.js
@@ -3,7 +3,14 @@ const MissingNumber = (a) => {
     for (let i = 0; i < a.length; i++) {
         sum = sum + a[i]
     }
-    console.log(sum)
+
+    let res = a.length * (a.length + 1) / 2
+    console.log(sum, res)
+
+    return res - sum
+
 }
 
 console.log(MissingNumber([3, 0, 2]))
+console.log(MissingNumber([0, 1]))
+console.log(MissingNumber([9,6,4,2,3,5,7,0,1]))
