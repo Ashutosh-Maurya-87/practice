d25ce27112ea7ed5ff65a8b392d0ff43abdee59b
Ashutosh Maurya <ashumaurya486@gmail.com>
2025-09-01 07:24:34 +0530

    define loop and parameter in the func

--- MissingNumber.js
+++ MissingNumber.js
@@ -1,5 +1,9 @@
-const MissingNumber = () => {
-
+const MissingNumber = (a) => {
+    let sum = 0
+    for (let i = 0; i < a.length; i++) {
+        sum = sum + a[i]
+    }
+    console.log(sum)
 }
 
-console.log(MissingNumber([3,0,2]))
+console.log(MissingNumber([3, 0, 2]))
