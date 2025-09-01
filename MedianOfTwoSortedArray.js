fcdd3f5aa5ef1ed35ed3eb090f6a5d1cf28fc2b3
Ashutosh Maurya <ashumaurya486@gmail.com>
2025-09-01 07:47:42 +0530

    changes in median

--- MedianOfTwoSortedArray.js
+++ MedianOfTwoSortedArray.js
@@ -1,5 +1,6 @@
 const MedianOfSortedArray = (a1, a2) => {
     let median, res = [], i = 0, j = 0
+    // concating array using while loop without any inbuilt nethod in ascending order
     while (i < a1.length && j < a2.length) {
         if (a1[i] < a2[j]) {
             res.push(a1[i])
@@ -9,8 +10,8 @@ const MedianOfSortedArray = (a1, a2) => {
             j++
         }
     }
-    res = res.concat(a1.slice(i)).concat(a2.slice(j))
-
+    res = res.concat(a1.slice(i)).concat(a2.slice(j)) // its concat the array by sorting
+    console.log(res)
     let mid = Math.floor(res.length / 2)
     if (res.length % 2 === 0) {
         median = (res[mid] + res[mid - 1]) / 2
