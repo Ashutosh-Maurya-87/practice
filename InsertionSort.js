44effa7dc9092ce9b5f44a00b242d73960ebd362
Ashutosh Maurya <ashumaurya486@gmail.com>
2025-09-02 08:42:15 +0530

    created to pointer variable which point the current and previous value and index respectively

--- InsertionSort.js
+++ InsertionSort.js
@@ -1,5 +1,11 @@
 const INsertionSort = (a) => {
-   
+    for (let i = 1; i < a.length; i++) {
+        let curr = a[i] // current element
+        let prev = i - 1    // previous index of array
+        
+    //    console.log(a, prev, curr)
+    }
+    return a
 }
 
 console.log(INsertionSort([4, 2, 3, 1]))
